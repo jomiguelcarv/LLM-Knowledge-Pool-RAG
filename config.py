46ecca81939b51f0e325fbfaeb48a5a4c29c2619
joaomigsilva <joao-mig-silva@hotmail.com>
2024-05-09 15:11:15 +0100

    minor changes

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import random
 from openai import OpenAI
 from keys import *
 
-# Server
+# LM Studio Server
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
 # Models
-
 embedding_model = "nomic-ai/nomic-embed-text-v1.5-GGUF"
 
 local_mistral_8x7b = [
@@ -28,6 +27,7 @@ westlake = [
         }
 ]
 
+# Notice how this model is not running locally. It uses an OpenAI key.
 gpt4_turbo = [{
         "model": "gpt-4-turbo-preview",
         "api_key": OPENAI_API_KEY,
@@ -54,3 +54,13 @@ llama3 = [
         }
 ]
 
+# If you download any new models, make sure to add its configuration here. Simply change the "model" name to the correct one.
+# myNewModel = [
+#         {
+#             "model": <the model name that you can find in LM Studio>,
+#             'api_key': 'any string here is fine',
+#             'api_type': 'openai',
+#             'base_url': "http://localhost:1234/v1",
+#             "cache_seed": random.randint(0, 100000),
+#         }
+# ]
